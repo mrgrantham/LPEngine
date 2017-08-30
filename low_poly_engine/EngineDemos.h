2536c68ea96e18536bf02e33cf285f6f915d960d
James Grantham <james.grantham@gmail.com>
2017-08-29 20:37:01 -0700

    Add bools to indicate if various continuous transformations should be running

--- low_poly_engine/EngineDemos.h
+++ low_poly_engine/EngineDemos.h
@@ -14,8 +14,13 @@
 @interface EngineDemos : NSObject
 
 @property (nonatomic, strong) EngineModel* arwing;
+@property (nonatomic, assign) BOOL rotateContinuous;
+@property (nonatomic, assign) BOOL translateContinuous;
+@property (nonatomic, assign) BOOL scaleContinuous;
+
 
 + (id)sharedManager;
+- (id) init;
 - (void) triangleDemo;
 - (void) arwingDemo;
 
