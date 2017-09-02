7e84e2cdb4b6a1251629a065a640eaaea3c3bfdc
James Grantham <james.grantham@gmail.com>
2017-09-02 15:13:57 -0700

    Rename EngineModelInterface to LPEngineModelInterface

---
+++ low_poly_engine/LPEngineModelInterface.m
@@ -0,0 +1,13 @@
+//
+//  EngineModelInterface.m
+//  low_poly_engine
+//
+//  Created by DEV on 8/20/17.
+//  Copyright © 2017 DEV. All rights reserved.
+//
+
+#import "LPEngineModelInterface.h"
+
+@implementation LPEngineModelInterface
+
+@end
