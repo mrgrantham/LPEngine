8677daeb34847be905ff1ad837829f0d634eb00d
James Grantham <james.grantham@gmail.com>
2017-09-02 17:59:38 -0700

    create properties object for storying persistent state of each animation

---
+++ low_poly_engine/LPEngineAnimationProperties.h
@@ -0,0 +1,25 @@
+//
+//  LPEngineAnimationProperties.h
+//  low_poly_engine
+//
+//  Created by DEV on 9/2/17.
+//  Copyright © 2017 DEV. All rights reserved.
+//
+
+#ifndef LPENGINEANIMATIONPROPERTIES_H
+#define LPENGINEANIMATIONPROPERTIES_H
+
+#import <Foundation/Foundation.h>
+
+@interface LPEngineAnimationProperties : NSObject
+
+@property (nonatomic, assign) NSInteger duration;
+@property (nonatomic, assign) NSInteger startValue;
+@property (nonatomic, assign) NSInteger endValue;
+@property (nonatomic, assign) NSInteger progress;
+@property (nonatomic, assign) NSInteger output;
+@property (nonatomic, assign) BOOL complete;
+
+@end
+
+#endif
---
+++ low_poly_engine/LPEngineAnimationProperties.m
@@ -0,0 +1,13 @@
+//
+//  LPEngineAnimationProperties.m
+//  low_poly_engine
+//
+//  Created by DEV on 9/2/17.
+//  Copyright © 2017 DEV. All rights reserved.
+//
+
+#import "LPEngineAnimationProperties.h"
+
+@implementation LPEngineAnimationProperties
+
+@end
