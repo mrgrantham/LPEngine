dfdb8984ccfebc667ed9c9e1aafc6266a449b2a4
James Grantham <james.grantham@gmail.com>
2017-09-05 12:16:22 -0700

    add resize of engine in response to a change in device orientation

--- LPEngine iOS/ViewController.m
+++ LPEngine iOS/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "LPEnginePrimitives.h"
 
 @interface ViewController ()
 
@@ -24,6 +25,34 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id <UIViewControllerTransitionCoordinator>)coordinator
+{
+    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+    
+    NSLog(@"turning");
+    
+    [[LPEnginePrimitives sharedManager] setVirtualWidth:size.width];
+    [[LPEnginePrimitives sharedManager] setVirtualHeight:size.height];
+    [[LPEnginePrimitives sharedManager] centerCamera];
+    
+    // Code here will execute before the rotation begins.
+    // Equivalent to placing it in the deprecated method -[willRotateToInterfaceOrientation:duration:]
+    
+    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+        
+        // Place code here to perform animations during the rotation.
+        // You can pass nil or leave this block empty if not necessary.
+        
+    } completion:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+        
+        // Code here will execute after the rotation has finished.
+        // Equivalent to placing it in the deprecated method -[didRotateFromInterfaceOrientation:]
+        
+    }];
+}
+
+
 
 
 @end
