d95a0df9d38afcf5ed39771f203fb731669207f3
James Grantham <james.grantham@gmail.com>
2017-09-06 20:34:47 -0700

    Introduce state for models added to scene to track transforms and lighting in scene space

---
+++ low_poly_engine/LPEngineSceneModelState.h
@@ -0,0 +1,32 @@
+//
+//  LPEngineModelSceneState.h
+//  low_poly_engine
+//
+//  Created by DEV on 9/6/17.
+//  Copyright © 2017 DEV. All rights reserved.
+//
+
+#ifndef LPENGINESCENEMODELSTATE_H
+#define LPENGINESCENEMODELSTATE_H
+
+#import <Foundation/Foundation.h>
+#import "LPEngineModel.h"
+#import "LPEngineTransforms.h"
+#import "LPEngineTransformState.h"
+#import "LPEnginePrimitives.h"
+
+@interface LPEngineSceneModelState : NSObject
+
+@property (nonatomic, strong) LPEngineModel *model;
+@property (nonatomic, strong) LPEngineTransformState *transformState;
+@property (nonatomic, assign) LPPoint *transformedVertices;
+@property (nonatomic, assign) float *lightFactors;
+@property (nonatomic, assign) LPPoint *normals;
+@property (nonatomic, assign) LPPoint modelCenter;
+
+- (id)init NS_UNAVAILABLE;
+- (id)initWithModel:(LPEngineModel*)model NS_DESIGNATED_INITIALIZER;
+
+@end
+
+#endif
---
+++ low_poly_engine/LPEngineSceneModelState.m
@@ -0,0 +1,28 @@
+//
+//  LPEngineModelSceneState.m
+//  low_poly_engine
+//
+//  Created by DEV on 9/6/17.
+//  Copyright © 2017 DEV. All rights reserved.
+//
+
+#import "LPEngineSceneModelState.h"
+#import "LPEngineTransformState.h"
+@implementation LPEngineSceneModelState
+
+- (id)initWithModel:(LPEngineModel*)model {
+    if (self = [super init]) {
+        _model = model;
+        _normals = malloc(sizeof(LPPoint) * model.faceCount);
+        _lightFactors = malloc(sizeof(float) * model.faceCount);
+        _transformedVertices = malloc(sizeof(LPPoint) * model.vertexCount);
+
+        _transformState = [[LPEngineTransformState alloc] init];
+        memset(_normals,0,sizeof(LPPoint) * model.faceCount);
+        memset(_lightFactors,0,sizeof(LPPoint) * model.faceCount);
+        memset(_transformedVertices,0,sizeof(LPPoint) * model.vertexCount);
+
+    }
+    return self;
+}
+@end
