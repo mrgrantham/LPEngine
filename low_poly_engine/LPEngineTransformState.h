c64d6eb62fef7ee0a1a95c4829f3905f4e5d3c5c
James Grantham <james.grantham@gmail.com>
2017-09-06 20:35:47 -0700

    add object for holding transform state for scene for all standard transforms

---
+++ low_poly_engine/LPEngineTransformState.h
@@ -0,0 +1,23 @@
+//
+//  LPEngineTransformState.h
+//  low_poly_engine
+//
+//  Created by DEV on 9/6/17.
+//  Copyright © 2017 DEV. All rights reserved.
+//
+
+#ifndef LPENGINETRANSFORMSTATE_H
+#define LPENGINETRANSFORMSTATE_H
+
+#import <Foundation/Foundation.h>
+#import "LPEnginePrimitives.h"
+
+@interface LPEngineTransformState : NSObject
+
+@property (nonatomic, assign) LPPoint rotation;
+@property (nonatomic, assign) LPPoint translation;
+@property (nonatomic, assign) LPPoint scale;
+
+@end
+
+#endif
---
+++ low_poly_engine/LPEngineTransformState.m
@@ -0,0 +1,16 @@
+//
+//  LPEngineTransformState.m
+//  low_poly_engine
+//
+//  Created by DEV on 9/6/17.
+//  Copyright © 2017 DEV. All rights reserved.
+//
+
+
+
+#import "LPEngineTransformState.h"
+
+@implementation LPEngineTransformState
+
+@end
+
